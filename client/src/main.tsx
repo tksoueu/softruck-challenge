cd449643ad923db69d0d8ba9d9bcc794d95da563
tksoueu <tkjardim@gmail.com>
2025-07-16 21:20:41 -0300

    adding leaflet

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,8 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/global.scss';
-import App from './App.tsx'
+import 'leaflet/dist/leaflet.css';
 import './i18n';
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
