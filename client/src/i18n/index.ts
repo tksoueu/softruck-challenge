dae023495f9cf825bb76cedfe4d820da9fb30079
tksoueu <tkjardim@gmail.com>
2025-07-18 00:13:15 -0300

    internalization with i18 + adding button to change lang

--- client/src/i18n/index.ts
+++ client/src/i18n/index.ts
@@ -2,11 +2,15 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import pt from './pt.json';
 import en from './en.json';
+import es from './es.json';
+import fr from './fr.json';
 
 i18n.use(initReactI18next).init({
   resources: {
     pt: { translation: pt },
-    en: { translation: en }
+    en: { translation: en },
+    es: {translation: es},
+    fr: {translation: fr},
   },
   lng: 'pt',
   fallbackLng: 'en',
